633497f8e2fb6179e668c8aadbcb08c8a8e27d8d
Quidvio <105707614+Quidvio@users.noreply.github.com>
2024-05-05 20:47:31 -0500

    forg

---
+++ Desertification Forge Mod/src/main/java/com/quidvio/deserts_and_dunes_desertification/DesertsAndDunesDesertification.java
@@ -0,0 +1,15 @@
+package com.quidvio.deserts_and_dunes_desertification;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(DesertsAndDunesDesertification.MODID)
+public class DesertsAndDunesDesertification {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "deserts_and_dunes_desertification";
+
+    public DesertsAndDunesDesertification() {
+
+    }
+}
